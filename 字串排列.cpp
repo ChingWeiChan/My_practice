92d0c773b49a7a63cd93166aedbed771cf17dc8a
ChingWeiChan <willy756.taiwan@gmail.com>
2016-10-20 13:07:38 +0800

    10/20 Change
    i, j , tmp  
    how to control?

--- 字串排列.cpp
+++ 字串排列.cpp
@@ -2,26 +2,31 @@
 #include<stdlib.h>
 #include<string.h>
 #include <ctype.h>
-void transfer(char *str[],int length,int* num[]);
+void transfer(char str[],int length,int num);
 int main(void){
 	int i;
-	char *str[1000];
+	char str[1000];
 	printf("Input the String:");
-    gets(*str);
-    int length=strlen(*str);
-    int *num[length+1];
+    scanf("%s",&str);
+    int length=strlen(str);
+    int num[length];
     transfer(str,length,num);
-   // printf("%d",length); //test
-//	puts(*str); //	test
+  //  printf("%d\n",length); //test
+//	puts(str); //	test
 	
 
 	return 0;
 } 
-void transfer(char *str[],int length,int *num[]){
+ void transfer(char str[],int length,int num[]){
+ 	int i,tmp,j;
 	for(i=0;i<length;i++){
+		tmp=0;
 		if(ispunct(str[i])){
-			
+		   	temp=i;
+			   for(j=0;j<i;j++){
+			   	  num[j]=(str[i-j-1]-48)*i;
+			   }	
 		}
 	}
 	
-}
+}    
