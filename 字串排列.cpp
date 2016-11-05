dc7fd00d955aa353786dbfadd91b756a16343885
ChingWeiChan <willy756.taiwan@gmail.com>
2016-11-05 10:49:14 +0800

    11/5 字串分割
    己成功字串分割，但尚未排列
    須以副程式(函數)帶入，再丟回main

--- 字串排列.cpp
+++ 字串排列.cpp
@@ -2,6 +2,7 @@
 #include<stdlib.h>
 #include<string.h>
 #include <ctype.h>
+/*
 void transfer(char str[],int length,int num[]);
 int main(void){
 	int i;
@@ -22,11 +23,80 @@ int main(void){
 	for(i=0;i<length;i++){
 		tmp=0;
 		if(ispunct(str[i])){
-		   	temp=i;
-			   for(j=0;j<i;j++){
-			   	  num[j]=(str[i-j-1]-48)*i;
-			   }	
+		   	tmp=i;
+			 
 		}
 	}
 	
 }    
+ */
+/*
+ int main(void){
+ 	int i=0;
+ 
+	 //LOOP
+ //	char str[1000];
+ //	puts(str); //test
+ //	int tmp=strlen(str);
+ //	char num[100][100];
+    char str[100];	
+    char num[99][100];
+//	char num[100]; 
+    gets(str);
+  char *cut;
+  cut=strtok(str,",");
+  
+   while(cut!=NULL){
+   	i++;
+   	strcpy(num[i],cut);
+   	printf("%s\n",num[i]);
+	   cut=strtok(NULL,",");
+	   printf("cut:%x\n",&cut);
+   }
+   
+// printf("%d",num[i]);
+ return 0;
+ 	
+ 	
+ }
+ */
+ 
+ 
+ int main(void){
+ int i=0,j=0;
+ int len,tot=1;
+ char str[100];	
+ char num[99][100];
+
+
+  gets(str);  
+  len=strlen(str);
+ 
+ for(i=0;i<len;i++)  
+ {
+   if(str[i]==',') 
+   {
+   	
+   	num[tot][j]=0;
+   	tot++;
+   	j=0;
+	   }	
+	   else
+ 	 	num[tot][j++]=str[i];
+ }
+
+  num[tot][j]=0;
+  
+  
+   
+ for(i=1;i<=tot;i++)   
+ 	printf("%d: %s \n",i,num[i]);
+ 	
+
+ return 0;
+ 	
+ 	
+ }
+ 
+ 
+ 
