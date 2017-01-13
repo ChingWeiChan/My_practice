66c0646b5388f1691ed51671cae65d0ebcbcdbf1
ChingWeiChan <willy756.taiwan@gmail.com>
2017-01-13 20:35:52 +0800

    update 1/13 修改幾行bug

--- 高中生程式解題系統/a017.c
+++ 高中生程式解題系統/a017.c
@@ -14,7 +14,7 @@ int main(void) {
 	cut=strtok(arr, space);
 	strcpy(tmp[i],cut);
 	while(cut!=NULL) {
-		printf("%d : %s\n",i,tmp[i]);
+		printf("%d : %s\n",i,tmp[i]);   //test
 		cut=strtok(NULL,space);
 		if (cut!=NULL) {
 			strcpy(tmp[++i],cut);
@@ -35,18 +35,40 @@ int main(void) {
 			case'/':
 				if(tmp[i-2][0]=='/') {
 					sum/=atoi(tmp[i+1]);
-				} else{
-				sum+=atoi(tmp[i-1])/atoi(tmp[i+1]);
-				 }
+				} else {
+					sum+=atoi(tmp[i-1])/atoi(tmp[i+1]);
+				}
 				break;
 			case'%':
 				sum+=atoi(tmp[i-1])%atoi(tmp[i+1]);
 				break;
 			case'+':
-				sum+=atoi(tmp[i+1]);
+				if(tmp[i+2][0]!='*' && tmp[i+2][0]!='/') {
+					if(i==1) {
+						sum+=atoi(tmp[i-1])+atoi(tmp[i+1]);
+					} else {
+						sum+=atoi(tmp[i+1]);
+					}
+				} else {
+					if(i==1) {
+						sum+=atoi(tmp[i-1]);
+					}
+				}
+
 				break;
 			case'-':
-				sum-=atoi(tmp[i+1]);
+				if(tmp[i+2][0]!='*' && tmp[i+2][0]!='/') {
+
+					if(i==1) {
+						sum+=atoi(tmp[i-1])-atoi(tmp[i+1]);
+					} else {
+						sum-=atoi(tmp[i+1]);
+					}
+				}else{
+					if(i==1) {
+						sum+=atoi(tmp[i-1]);
+					}
+				}
 				break;
 		}
 
