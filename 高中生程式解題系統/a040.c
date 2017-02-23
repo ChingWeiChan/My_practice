39e890a01024ab44e94f1c2cc89e3a4565353857
ChingWeiChan <willy756.taiwan@gmail.com>
2017-02-23 22:22:02 +0800

    註解

--- 高中生程式解題系統/a040.c
+++ 高中生程式解題系統/a040.c
@@ -2,27 +2,28 @@
 #include<stdlib.h>
 #include<string.h>
 #include<math.h>
-int Armstrong(char*);
-void number(char*);
+int Armstrong(char*); //transfer
+void number(char*);   //num++
 int main(void){
-	int i,flag;
-    char min[10],max[10],num[10];
+	int i,flag; 
+    char min[10],max[10],num[10];   //num is the value from min to max 
     while(scanf("%s",&min)!=EOF){
     scanf("%s",&max);
     
-    strcpy(num,min);
-    flag=0;
+    strcpy(num,min);   //num starts from the value of min
+    flag=0;   //control if
+	 
     for(i=0;i<(atoi(max)-atoi(min)+1);i++){
     	if(atoi(num)==Armstrong(num)){
     		printf("%s ",num);
-    		flag=1;
+    		flag=1;  //if flag=1 then must have  Armstrong number
 		}
 		if(i==(atoi(max)-atoi(min))-1){
 		 if(flag==0)printf("none\n");
 		 else if(flag==1)printf("\n");
 		
 	}
-		number(num);
+		number(num); //num ++
 		
 	}	
 }
@@ -30,7 +31,8 @@ int main(void){
 }
 int Armstrong(char* tmp){
 	
-	int i,j,digit=strlen(tmp),sum=0;
+	int i,j,digit=strlen(tmp); //number digit 
+	int sum=0;
 	for(i=0;i<digit;i++){
 			sum+=pow(tmp[i]-48,digit);
 	}
