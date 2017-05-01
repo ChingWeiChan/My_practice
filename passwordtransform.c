17c3ecf4ec30283d7b698a4d7ab74ccdca4c8b45
ChingWeiChan <willy756.taiwan@gmail.com>
2017-05-01 23:32:38 +0800

    排版~~

--- passwordtransform.c
+++ passwordtransform.c
@@ -5,30 +5,24 @@ int main(int argc, char const *argv[]) {
         int i,j;
         char input[100];
         scanf("%s",&input);
-        if (strlen(input)%2==0) {
-                j=1;
-        }else{
-                j=0;
-        }
-        for (i = strlen(input)-1; i >=0; i--) {
-                if ( (i+j)%2==0 || (i+j)==0 ) {
-                        if (input[i]=='9') {
-                                input[i]='0';
-                        } else {
-                                input[i]++;
-                        }
-                } else {
-                        if (input[i]=='0') {
-                                input[i]='9';
-                        }else{
-                                input[i]--;
-                        }
 
-                }
+        if  (strlen(input)%2==0) j=1;  else j=0;
+
+        for (i = strlen(input)-1; i >=0; i--) {
+
+                if ( (i+j)%2==0 || (i+j)==0 ) {
+                        if (input[i]=='9') input[i]='0';
+                        else               input[i]++;
+              }
+              else
+              {
+                        if (input[i]=='0') input[i]='9';
+                        else               input[i]--;
+              }
         }
-        for (size_t i = 0; i < strlen(input); i++) {
-                printf("%c",input[i] );
-        }
+
+        printf("\n");
+        for (i = 0; i < strlen(input); i++) printf("%c",input[i] );
         printf("\n");
         return 0;
 }
