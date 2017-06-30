f9c5e93d8214847aec0aa72017ff764dcd7c6c1d
ChingWeiChan <willy756.taiwan@gmail.com>
2017-06-30 22:12:17 +0800

    yang_tze.py big update!!!!!

--- PythonPractice/Gaussian_Elimination.py
+++ PythonPractice/Gaussian_Elimination.py
@@ -10,3 +10,7 @@ start time:2017/6/29 20:40
 #from numpy import array
 m,n=input().split(' ') # input an  m x n matrix A
 Matrix=[input().split(' ') for x in range(int(m))]  # initialization
+for i in range(0,m):
+    for j in range(0,n):
+        
+        
--- PythonPractice/Yang_Tze.py
+++ PythonPractice/Yang_Tze.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
-import requests,re
+import requests,re,time
+import matplotlib.pyplot as plt
+import numpy as np
 from bs4 import BeautifulSoup
 url=[]
 total=[]
 i=0
-dict1={"學務處":0,"教務處":0,"體育組":0,"圖書組":0,"註冊組":0,"體育組":0,"應外科":0,"輔導室":0}
+dict1={"學務處":0,"教務處":0,"體育組":0,"圖書組":0,"註冊組":0,"體育組":0,"應外科":0,"輔導室":0} #總人數
+dict2={"學務處":0,"教務處":0,"體育組":0,"圖書組":0,"註冊組":0,"體育組":0,"應外科":0,"輔導室":0}                                                                                         #總文章數
 listkey=list(dict1.keys())
 for num in range(1,7):
     url.append("http://www.ytjh.ylc.edu.tw/news/3?page={}".format(num))
@@ -27,7 +30,39 @@ for num in range(1,7):
             #print(dict1[listkey[i]])
             if re.compile(r'單位 : (.*)').search(data2[data]).group(1)==listkey[i]:
                 dict1[listkey[i]]=int(re.compile('\d+').search(data3[data]).group())+int(dict1[listkey[i]])
-listvalue=list(dict1.values())
+                dict2[listkey[i]]=int(dict2[listkey[i]])+1
+list1_value=list(dict1.values())
+list2_value=list(dict2.values())
 print('\n')
 for i in range(len(listkey)):
-        print("%s : %d" % (listkey[i],listvalue[i]))
+        print("%s : %d" % (listkey[i],list1_value[i]))
+
+#------------draw picture---------------------------------------------------------------------------------
+xticks=np.arange(len(listkey))+1
+def visitor(xticks):
+    plt.figure(1)               
+    plt.bar(xticks,list1_value,align='center')
+    plt.ylim(0,10000)
+    j=1
+    for i in list1_value:
+       plt.text(j,i+100,"%d"%i,horizontalalignment='center')
+       j+=1
+    plt.xticks(xticks,listkey)
+    plt.title("%s 揚子中學各處室網頁點閱總人數" %(time.strftime("%m/%d")))
+    plt.savefig("visitor.png",dpi=300,format="png")
+def article(xticks):
+    plt.figure(2)
+    plt.bar(xticks,list2_value)
+    plt.ylim(0,30)
+    j=1
+    for i in list2_value:
+       plt.text(j,i+2,"%d"%i,horizontalalignment='center')
+       j+=1
+    
+    plt.xticks(xticks,listkey)
+    plt.title("%s 揚子中學各處室網頁文章數" %(time.strftime("%m/%d")))
+    plt.savefig("article.png",dpi=300,format="png")
+plt.figure(1)
+plt.figure(2)
+visitor(xticks)
+article(xticks)
